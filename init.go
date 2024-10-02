a6e030effffe11373eb00f86bf79f2d8b473171c
Danny De Novi <dannydenovi@gmail.com>
2024-10-02 19:52:55 +0200

    added init

---
+++ init.go
@@ -0,0 +1,16 @@
+package circular_protocol_api
+
+import (
+	"fmt"
+)
+
+// Global variables or configurations can be declared here
+var (
+	ApiVersion = "1.0.8"
+)
+
+// Init function to set up the package
+func init() {
+	// Example of any initialization logic you want to execute
+	fmt.Println("Initializing Circular Protocol API, version:", ApiVersion)
+}
